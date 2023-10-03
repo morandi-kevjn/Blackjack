896675d65bc45dfe484de4b3eefc58fb2c80d651
morandi-kevjn <85896336+morandi-kevjn@users.noreply.github.com>
2023-10-03 05:29:35 +0200

    Third step into this project.

--- main.py
+++ main.py
@@ -58,5 +58,58 @@ class Deck:
         return cards_dealt
 
 
-card1 = Card('hearts', {'rank': 'J', 'value': 10})
-print(card1)
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+
+        for card in self.cards:
+            card_value = int(card.rank['value'])
+            self.value += card_value
+            if card.rank['rank'] == 'A':
+                has_ace = True
+
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def is_blackjack(self):
+        return self.get_value() == 21
+
+    def display(self, show_all_dealer_cards=False):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand: ''')
+
+        for index, card in enumerate(self.cards):
+            if index == 0 and self.dealer \
+                    and not show_all_dealer_cards and not self.is_blackjack():
+                print("hidden")
+            else:
+                print(card)
+
+        if not self.dealer:
+            print('Value: ', self.get_value())
+
+        print()
+
+
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))
+
+# print(hand.cards[0], hand.cards[1])
+hand.display()
+
+hand.get_value()
